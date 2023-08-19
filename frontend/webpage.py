97e795c23132dcfa2f87d43dfff8e5a7119ed89a
UltimateForce21 <abdullahmohammad155@gmail.com>
2023-08-19 14:03:51 -0400

    Creating login page

--- frontend/webpage.py
+++ frontend/webpage.py
@@ -1,54 +1,26 @@
 from taipy.gui import Gui, notify
 import pandas as pd
 
-text = "Original text"
+email = ""
+password = ""
 
-list_to_display = [100/x for x in range(1, 100)]
 
-dataframe = pd.DataFrame({"Text":['Test', 'Other', 'Love'],
-                          "Score Pos":[1, 1, 4],
-                          "Score Neu":[2, 3, 1],
-                          "Score Neg":[1, 2, 0],
-                          "Overall":[0, -1, 4]})
 
-property_chart = {"type":"bar",
-                  "x":"Text",
-                  "y[1]":"Score Pos",
-                  "y[2]":"Score Neu",
-                  "y[3]":"Score Neg",
-                  "y[4]":"Overall",
-                  "color[1]":"green",
-                  "color[2]":"grey",
-                  "color[3]":"red",
-                  "type[4]":"line"
-                 }
 
 # Definition of the page
-page = """
-# Getting started with Taipy GUI
+page = """The text is:
+# Login
 
-My text: <|{text}|>
+Email: <|{email}|input|>
 
-<|{text}|input|>
+Password: <|{password}|input|>
 
-<|Run local|button|on_action=on_button_action|>
-
-<|{list_to_display}|chart|>
-
-<|{dataframe}|table|>
-
-<|{dataframe}|chart|properties={property_chart}|>
+<|Run local|button|on_action=loginButtonAction|>
 
 """
 
-def on_button_action(state):
-    notify(state, 'info', f'The text is: {state.text}')
-    state.text = "Button Pressed"
-
-def on_change(state, var_name, var_value):
-    if var_name == "text" and var_value == "Reset":
-        state.text = ""
-        return
+def loginButtonAction(state):
+    notify(state, 'Login:', "Running Login Function")
 
 
 Gui(page).run(use_reloader=True)
