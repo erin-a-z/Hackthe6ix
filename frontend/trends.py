aff61c415ffbb77d63ec8941255429011776421b
erin-a-z <ezzeuu@gmail.com>
2023-08-20 08:17:17 -0400

    scan qr code

--- frontend/trends.py
+++ frontend/trends.py
@@ -1,6 +1,6 @@
 from pytrends.request import TrendReq
 
 pytrends = TrendReq(hl='en-US', tz=360)
-pytrends.build_payload(kw_list=['pizza', 'bagel'], timeframe=['2022-09-04 2022-09-10', '2022-09-18 2022-09-24'])
+pytrends.build_payload(kw_list=['recycling', 'garbage'], timeframe=['2022-09-10 2023-08-10', '2022-09-10 2023-08-10'])
 wow = pytrends.multirange_interest_over_time()
 print(wow)
