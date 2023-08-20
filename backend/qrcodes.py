e74f061ca173c8b3b37cddc8f8c1c12b048ad2f3
Stafidhe <21gpcs@queensu.ca>
2023-08-19 22:54:33 -0700

    qrcodeScanning

--- backend/qrcodes.py
+++ backend/qrcodes.py
@@ -1,44 +1,29 @@
 import cv2
 import qrcode
-import math
+
+# List of expected QR code data values
+expected_qr_data = ["23452345", "45879845"]
+
+# List to keep track of scanned QR codes
+scanned_qr_codes = []
 
 def checkqr(qrscan):
-    expected_qr_data_1 = 1
-    expected_qr_data_2 = 2
+    global scanned_qr_codes
 
-    qrlists = ["1","2"]
-
-
-    if qrscan == expected_qr_data_1:
-        qrscan = "BOTTLE1"
-        qrlists.remove("1")
-    elif qrscan == expected_qr_data_2: 
-        qrscan = "BOTTLE2"
-        qrlists.remove("2")
-    else:
-        print("Invalid scan")
-
-    scanned_image = cv2.imread("scanned_qrcode.png")
-    print("Image loaded")
-    
-    detector = cv2.QRCodeDetector()
-    print("Detector initialized")
-    
-    retval, decoded_info, points, qr_code = detector.detectAndDecode(scanned_image)
-    print("QR code detected and decoded")
-
-    if retval:
-        if decoded_info == expected_qr_data_1:
-            print("Scanned QR Code matches BOTTLE1.")
-        elif decoded_info == expected_qr_data_2:
-            print("Scanned QR Code matches QR Code 2.")
+    # Check if the scanned QR code matches any expected value
+    if qrscan in expected_qr_data:
+        if qrscan in scanned_qr_codes:
+            print(f"QR Code {qrscan} has already been recycled.")
         else:
-            print("Scanned QR Code does not match any expected QR code.")
+            scanned_qr_codes.append(qrscan)
+            print(f"QR Code {qrscan} has been scanned. Recycle now.")
     else:
-        print("QR code invalid")
+        print("Invalid scan.")
 
-# Print statement to indicate the function is reached 
-    print("Function reached")
+    # Print the list of scanned QR codes
+    #print("Scanned QR Codes:", scanned_qr_codes)
 
-    return 
-# Replace this value with the actual scanned data
+# Call the function with the scanned QR code data
+checkqr("23452345") 
+checkqr("45879845")  
+checkqr("45879845")  
